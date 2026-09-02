fb2c6bcd5e8a2f9fd8743ba4fc2acabf5003bb3a
pingupengi2010 <93376708+PinguPeng2010@users.noreply.github.com>
2026-09-02 18:39:27 +0100

    nothing

--- jorge/indexer.py
+++ jorge/indexer.py
@@ -1,11 +1,14 @@
 from zstandard import ZstdDecompressor
 from pathlib import Path
-
+import sqlite3
+from multiprocessing import Queue
 decompressor = ZstdDecompressor()
 
 BASE_DIR = Path(__file__).resolve().parent
-DB_PATH = BASE_DIR / "../data/gurt.db" 
+DB_PATH = BASE_DIR / "../data/jorgurt.db" 
 
 
-def getUrls():
-    
+# jorge stuff
+# Each page needs to be taken from the db.
+# Then decompress the page from pages table
+# Parse the text from each page, and 
